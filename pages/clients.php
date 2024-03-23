d40e91ab5098e969b3b57d02034b5ff4b5e0347e
o-psi <andymalsb@gmail.com>
2024-03-23 17:38:12 +0000

    finish inc_all  cleanup for new structure

--- pages/clients.php
+++ pages/clients.php
@@ -324,7 +324,7 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
 
                         <?php
 
-                        require "var/www/develop.twe.tech/includes/modals/client_edit_modal.php";
+                        require "/var/www/develop.twe.tech/includes/modals/client_edit_modal.php";
 
 
                     } ?>
@@ -332,15 +332,15 @@ $num_rows = mysqli_fetch_row(mysqli_query($mysqli, "SELECT FOUND_ROWS()"));
                     </tbody>
                 </table>
             </div>
-            <?php require_once "var/www/develop.twe.tech/includes/pagination.php";
+            <?php require_once "/var/www/develop.twe.tech/includes/pagination.php";
  ?>
         </div>
     </div>
 
 <?php
-require_once "var/www/develop.twe.tech/includes/modals/client_add_modal.php";
+require_once "/var/www/develop.twe.tech/includes/modals/client_add_modal.php";
 
-require_once "var/www/develop.twe.tech/includes/modals/client_export_modal.php";
+require_once "/var/www/develop.twe.tech/includes/modals/client_export_modal.php";
 
-require_once "var/www/develop.twe.tech/includes/footer.php";
+require_once "/var/www/develop.twe.tech/includes/footer.php";
 
