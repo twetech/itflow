899054233d9ba6b8fcbc6b21f94f692a376776ad
o-psi <andymalsb@gmail.com>
2024-06-05 15:29:58 -0500

    Reusable error page

---
+++ src/View/error.php
@@ -0,0 +1,102 @@
+<!doctype html>
+
+<html
+  lang="en"
+  class="light-style layout-wide customizer-hide"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="/includes/assets/"
+  data-template="horizontal-menu-template">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>Error - Pages | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="/includes/assets/img/favicon/favicon.ico" />
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+      rel="stylesheet" />
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="/includes/assets/vendor/fonts/boxicons.css" />
+    <link rel="stylesheet" href="/includes/assets/vendor/fonts/fontawesome.css" />
+    <link rel="stylesheet" href="/includes/assets/vendor/fonts/flag-icons.css" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="/includes/assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="/includes/assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="/includes/assets/css/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="/includes/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="/includes/assets/vendor/libs/typeahead-js/typeahead.css" />
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="/includes/assets/vendor/css/pages/page-misc.css" />
+
+    <!-- Helpers -->
+    <script src="/includes/assets/vendor/js/helpers.js"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
+    <script src="/includes/assets/vendor/js/template-customizer.js"></script>
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="/includes/assets/js/config.js"></script>
+  </head>
+
+  <body>
+    <!-- Content -->
+
+    <!-- Error -->
+    <div class="container-xxl container-p-y">
+      <div class="misc-wrapper">
+        <h2 class="mb-2 mx-2"><?=$title?></h2>
+        <p class="mb-4 mx-2"><?=$message?></p>
+        <a href="/public/" class="btn btn-primary">Back to home</a>
+        <div class="mt-3">
+          <img
+            src="/includes/assets/img/illustrations/page-misc-error-light.png"
+            alt="page-misc-error-light"
+            width="500"
+            class="img-fluid"
+            data-app-dark-img="illustrations/page-misc-error-dark.png"
+            data-app-light-img="illustrations/page-misc-error-light.png" />
+        </div>
+      </div>
+    </div>
+    <!-- /Error -->
+
+    <!-- / Content -->
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+
+    <script src="/includes/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="/includes/assets/vendor/libs/popper/popper.js"></script>
+    <script src="/includes/assets/vendor/js/bootstrap.js"></script>
+    <script src="/includes/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="/includes/assets/vendor/libs/hammer/hammer.js"></script>
+    <script src="/includes/assets/vendor/libs/i18n/i18n.js"></script>
+    <script src="/includes/assets/vendor/libs/typeahead-js/typeahead.js"></script>
+    <script src="/includes/assets/vendor/js/menu.js"></script>
+
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+    <!-- Main JS -->
+    <script src="/includes/assets/js/main.js"></script>
+
+    <!-- Page JS -->
+  </body>
+</html>
